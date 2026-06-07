06d5640a2db39bdef48b1b100d721083a0caa4f0
wechpisit2009pho-ctrl <wechpisit2009pho@gmail.com>
2026-06-07 16:50:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTUxMzExNTk0MzQ5NTEzOTMzOA.GTGKjd.-1W6IKb3DRhxfa5HTEwxICGleA-OpDQi4uUqFc",
   language: "en",
   ownerID: ["962994407651553302", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
